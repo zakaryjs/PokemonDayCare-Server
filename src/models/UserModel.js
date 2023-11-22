0b50a30817f72abe36aa39a40742d56e9ee7efcb
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-11-22 15:42:38 +0800

    created user model

---
+++ src/models/UserModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    lastName: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: false
+    }
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = { User }
