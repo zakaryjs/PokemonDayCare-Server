df16f0eb5a8562a122fdc822d286c56e3f860e6a
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-04 22:55:47 +0800

    fix: Cors cookie same site origin to none (#14)
    * added cors
    
    * added extra origin
    
    * enabled credentials
    
    * added https to origin
    
    * cors allow any
    
    * fix: origins
    
    * fix: added samesite none to cookie

--- src/server.js
+++ src/server.js
@@ -9,7 +9,10 @@ const cors = require('cors')
 const corsOptions = {
     origin: ["http://localhost:3000", "http://localhost:3000/", "https://localhost:3000", "https://localhost:3000/",],
 	optionsSuccessStatus: 200,
-    credentials: true
+    credentials: true,
+    cookie: {
+        sameSite: 'none'
+    }
 }
 app.use(cors(corsOptions));
 
