37235d62b7cf7d336545b71c2c54ff24df010d5d
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-04 22:35:24 +0800

    fix: cors allow any (#12)
    * added cors
    
    * added extra origin
    
    * enabled credentials
    
    * added https to origin
    
    * cors allow any

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const app = express()
 
 const cors = require('cors')
 const corsOptions = {
-    origin: ["http://localhost:3000", "http://localhost:3000/", "https://localhost:3000", "https://localhost:3000/",],
-	  optionsSuccessStatus: 200,
+    origin: '*',
+	optionsSuccessStatus: 200,
     credentials: true
 }
 app.use(cors(corsOptions));
