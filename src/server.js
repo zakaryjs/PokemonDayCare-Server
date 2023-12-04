71ce393d2a5ef833bcab5ea9cba81caf7a664196
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-04 22:18:03 +0800

    fix: Cors credentials (#10)
    * added cors
    
    * added extra origin
    
    * enabled credentials

--- src/server.js
+++ src/server.js
@@ -8,7 +8,8 @@ const app = express()
 const cors = require('cors')
 const corsOptions = {
     origin: ["http://localhost:3000", "http://localhost:3000/"],
-	optionsSuccessStatus: 200
+	optionsSuccessStatus: 200,
+    credentials: true
 }
 app.use(cors(corsOptions));
 
