cc2702a3347c02e8312848409a176305de3b1647
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-05 18:46:02 +0800

    add error handling to login

--- src/controllers/UserRoutes.js
+++ src/controllers/UserRoutes.js
@@ -42,28 +42,33 @@ router.post('/register', async (request, response) => {
 })
 
 router.post('/login', async (request, response) => {
-    let targetUser = await User.findOne({email: request.body.email}).exec()
+    try {
+        let targetUser = await User.findOne({email: request.body.email}).exec()
 
-    if (await validateHashedData(request.body.password, targetUser.password)){
-        let encryptedUserJwt = await generateUserJWT(
-            {
-                userID: targetUser.id,
-                email: targetUser.email,
-                password: targetUser.password,
-                isAdmin: targetUser.isAdmin
+        if (await validateHashedData(request.body.password, targetUser.password)){
+            let encryptedUserJwt = await generateUserJWT(
+                {
+                    userID: targetUser.id,
+                    email: targetUser.email,
+                    password: targetUser.password,
+                    isAdmin: targetUser.isAdmin
+                }
+            )
+            
+            response.cookie('jwt', encryptedUserJwt, {  maxAge: 7*24*60*60*1000, httpOnly: true, secure: true, sameSite: 'none' })
+            if (targetUser.isAdmin) {
+                response.cookie('isAdmin', true, {  maxAge: 7*24*60*60*1000, httpOnly: true, secure: true, sameSite: 'none' })
             }
-        )
-        
-        response.cookie('jwt', encryptedUserJwt, {  maxAge: 7*24*60*60*1000, httpOnly: true, secure: true, sameSite: 'none' })
-        if (targetUser.isAdmin) {
-            response.cookie('isAdmin', true, {  maxAge: 7*24*60*60*1000, httpOnly: true, secure: true, sameSite: 'none' })
+            response.json(encryptedUserJwt)
+            
+    
+        } else {
+            response.status(400).json({message:"Invalid user details provided."})
         }
-        response.json(encryptedUserJwt)
-        
-
-    } else {
+    } catch (error) {
         response.status(400).json({message:"Invalid user details provided."})
     }
+    
 })
 
 router.put('/:userID', jwtInHeader, verifyJwtRole, adminOnly, async (request, response) => {
