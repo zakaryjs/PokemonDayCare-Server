378cea7af67d0662fa952c36ea376aae6047805a
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-06 21:25:15 +0800

    feat: implemented functions required for portal functionality (#31)

--- src/controllers/UserRoutes.js
+++ src/controllers/UserRoutes.js
@@ -6,12 +6,14 @@ const { User } = require('../models/UserModel')
 
 const {
     encryptString, decryptString, decryptObject, hashString, validateHashedData, 
-    generateJWT, generateUserJWT, verifyUserJWT, createUser, updateUser, deleteUser, errorHandler
+    generateJWT, generateUserJWT, verifyUserJWT, createUser, updateUser, deleteUser, errorHandler, getSpecificUser
 } = require('./UserFunctions')
 
 const {
     jwtInHeader, verifyJwtRole, adminOnly
 } = require('../middleware/UserMiddleware')
+
+const jwt = require('jsonwebtoken')
 
 router.post('/register', async (request, response) => {
 
@@ -58,6 +60,7 @@ router.post('/login', async (request, response) => {
             )
             
             response.cookie('jwt', encryptedUserJwt, {  maxAge: 7*24*60*60*1000, httpOnly: true, secure: true, sameSite: 'none' })
+            response.cookie('isAdmin', false, {  maxAge: 7*24*60*60*1000, httpOnly: true, secure: true, sameSite: 'none' })
             if (targetUser.isAdmin) {
                 response.cookie('isAdmin', true, {  maxAge: 7*24*60*60*1000, httpOnly: true, secure: true, sameSite: 'none' })
             }
@@ -69,6 +72,46 @@ router.post('/login', async (request, response) => {
         }
     } catch (error) {
         response.status(400).json({message:"Invalid user details provided."})
+    }
+    
+})
+
+router.post('/logout', async (request, response) => {
+    response.cookie('jwt', 'jwtLogOut', {  maxAge: 1, httpOnly: true, secure: true, sameSite: 'none' })
+    response.cookie('isAdmin', false, {  maxAge: 1, httpOnly: true, secure: true, sameSite: 'none' })
+
+    response.json({message: 'logged out'})
+})
+
+router.post('/token-refresh', async(request, response) => {
+    try {
+        let oldToken = request.cookies.jwt;
+        let refreshResult = await verifyUserJWT(oldToken).catch(error => {return {error: error.message}})
+        let isAdmin = request.cookies.isAdmin
+        
+        let verifiedUserJwt = jwt.verify(refreshResult, process.env.JWT_SECRET, {complete: true})
+    
+        let decryptedJwtToParse = decryptString(verifiedUserJwt.payload.data)
+    
+        let parsedData = JSON.parse(decryptedJwtToParse)
+    
+        if (isAdmin === 'true') {
+            response.json({
+                jwt: refreshResult,
+                isAdmin: isAdmin,
+                user: parsedData
+            })
+        }
+        if (isAdmin === 'false') {
+            response.json({
+                jwt: refreshResult,
+                user: parsedData
+            });
+        }
+    } catch (error) {
+        response.json({
+            error: 'no user found'
+        })
     }
     
 })
@@ -96,6 +139,10 @@ router.put('/:userID', jwtInHeader, verifyJwtRole, adminOnly, async (request, re
 
 router.delete('/:userID', jwtInHeader, verifyJwtRole, adminOnly, async (request, response) => {
     response.json(await deleteUser(request.params.userID))
+})
+
+router.get('/:userID', async (request, response) => {
+    response.json(await getSpecificUser(request.params.userID));
 })
 
 module.exports = router
